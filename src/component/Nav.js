f9063bed9061ddd432a35a0ca9f698c160e75fb6
y645wu <y645wu@uwaterloo.ca>
2022-05-10 21:31:20 -0400

    webpage finished

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,16 +1,17 @@
 import {AiOutlineGithub, AiOutlineLinkedin} from "react-icons/ai";
 import { IconContext } from "react-icons";
+
 function Nav() {
     return (
-        <nav className="Nav col-12">
+        <nav id="Nav" className="Nav col-12">
             <h1 className="Nav-logo">{"Yiru"}</h1>
             <ul className="Nav-bar nav pt-sm-2 justify-content-end">
-                <li className="nav-item pe-sm-1"><a className="nav-link active" aria-current="page" href="#">HOME</a></li>
-                <li className="nav-item pe-sm-1"><a className="nav-link" href="#">WORKS</a></li>
-                <li className="nav-item pe-sm-1"><a className="nav-link" href="#">EDUCATIONS</a></li>
-                <li className="nav-item pe-sm-1"><a className="nav-link" href="#">PROJECTS</a></li>
-                <li className="nav-item pe-sm-1"><a className="nav-link" href="#">CONTACT ME</a></li>
-                <li className="nav-item pe-sm-1">
+                <li className="nav-item"><a className="nav-link" href="#Home">HOME</a></li>
+                <li className="nav-item"><a className="nav-link" href="#Work">WORKS</a></li>
+                <li className="nav-item"><a className="nav-link" href="#Education">EDUCATIONS</a></li>
+                <li className="nav-item"><a className="nav-link" href="#Project">PROJECTS</a></li>
+                <li className="nav-item"><a className="nav-link" href="#Contact">CONTACT ME</a></li>
+                <li className="nav-item">
                     <a target="_blank" rel="noreferrer" href="https://github.com/yiruW" className="nav-link-icon">
                         <IconContext.Provider value={{ color: "#000", className: "icon-link float-left" }}>
                         <div><AiOutlineGithub /></div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import $ from 'jquery';
+
 import Nav from './component/Nav';
 import Home from "./component/Home";
 import Work from "./component/Work";
 import Project from "./component/Project";
+import Contact from "./component/Contact";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +15,6 @@ root.render(
     <Home/>
     <Work/>
     <Project/>
+    <Contact/>
   </div>
-  
 );

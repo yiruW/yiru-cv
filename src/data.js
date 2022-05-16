42ce8919e3aeec6797a5850b99cb00eecafaa33a
Yiru Wu <y645wu@uwaterloo.ca>
2022-05-16 13:55:56 -0400

    update data

--- src/data.js
+++ src/data.js
@@ -1,17 +1,17 @@
 const work = [
     {key : 0, title: "Full Stack Developer", 
     company: "CI technologies", duration: "2022.1 - 2022.4",
-    skills: "Java, JavaScript, HTML, CSS, mySQL, Spring Boot, Vue, Amazon Saas"},
+    skills: "Java, JavaScript, HTML, CSS, MySQL, Spring Boot, Vue.js, RESTful, Amazon S3"},
     {key : 1, title: "Software Engineer Trainee",
     company: "China Unicom", duration: "2021.1 - 2021.4",
-    skills: "Java, React, Photoshop, Adobe illustrator"},
-    {key : 2, title: "Software Engineer", 
+    skills: "Java, JavaScript,  Photoshop, Adobe illustrator"},
+    {key : 2, title: "Software Development Engineering", 
     company: "Neusoft", duration: "2020.5 - 2020.8",
-    skills: "Java, Spring Boot, HTMl, CSS"},
+    skills: "Java, Spring Boot, HTML, CSS, React"},
     {key : 3, title: "QA Engineer", 
     company: "DarkMatter", duration: "2019.9 - 2019.12",
     skills: "Jira, Jenkins, Java, Xcode"},
-    {key : 4, title: "Software Engineer", 
+    {key : 4, title: "Software Development Engineering", 
     company: "Neusoft", duration: "2019.1 - 2019.4",
     skills: "Java, Spring Boot, mySQL"},
 ]
@@ -21,12 +21,12 @@ function brief(){
 }
 function education(){
     return ([
-        {key: 0, field: "Android Development", subline: "Online Course", duration: "2022-present", details: "Java, Permanent Data Storage, Kotlin"},
+        {key: 0, field: "Kafka Streams", subline: "Online Course", duration: "2022-present", details: "KStream, Ktable, Exactly Once"},
         {key: 1, field: "Distributed Computing", subline: "", duration: "2022-present", details: "Hadoop, Spark, ZooKeeper, Kafka, Thrift"},
         {key: 2, field: "Microservices", subline: "Online Course", duration: "2022-present", details: "Spring Cloud, RESTful web services, Kubernetes, Docker"},
         {key: 3, field: "Operation System", subline: "", duration: "2021", details: "C++, memory allocation, mode conversion"},
         {key: 4, field: "Web Design", subline: "Online Course", duration: "2021", details: "Figma, Photoshop, Adobe illustrator"},
-        {key: 5, field: "Web Development", subline: "", duration: "2019-2022", details: "HTML, CSS, JavaScript, Java, Spring Boot, Vue, React"} 
+        {key: 5, field: "Web Development", subline: "", duration: "2019-2022", details: "HTML, CSS, JavaScript, Java, Spring Boot, Vue, React, MySQL, MongoDB"} 
     ])
 }
 function project(){

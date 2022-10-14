141b56800dbc143375eddf5357b9bbd6bffe3091
Yiru Wu <y645wu@uwaterloo.ca>
2022-10-13 23:42:17 -0400

    Revert "update resume data"
    This reverts commit 025b0b93d56325b517e5127962fdca15793c5c36.

--- src/data.js
+++ src/data.js
@@ -1,23 +1,26 @@
 const work = [
-    {key : 0, title: "Full Stack Developer", 
+    {key: 0, title: "Software Developer",
+    company: "Vidyard", duration: "2022.9 - present",
+    skills: "Ruby on Rails, Vue.js, Ruby, JavaScript, MySQL, GraphQL, microservices, Docker, Kubernetes"},
+    {key : 1, title: "Full Stack Developer", 
     company: "CI technologies", duration: "2022.1 - 2022.4",
     skills: "Java, JavaScript, HTML, CSS, MySQL, Spring Boot, Vue.js, RESTful, Amazon S3"},
-    {key : 1, title: "Software Engineer Trainee",
+    {key : 2, title: "Software Engineer Trainee",
     company: "China Unicom", duration: "2021.1 - 2021.4",
-    skills: "Java, JavaScript,  Photoshop, Adobe illustrator"},
-    {key : 2, title: "Software Development Engineering", 
+    skills: "Spring Boot, React.js, Java, JavaScript, Photoshop, Adobe illustrator"},
+    {key : 3, title: "Software Developer", 
     company: "Neusoft", duration: "2020.5 - 2020.8",
-    skills: "Java, Spring Boot, HTML, CSS, React"},
-    {key : 3, title: "QA Engineer", 
+    skills: "Spring Boot, React.js, Java, JavaScript, HTML, CSS"},
+    {key : 4, title: "QA Engineer", 
     company: "DarkMatter", duration: "2019.9 - 2019.12",
-    skills: "Jira, Jenkins, Java, Xcode"},
-    {key : 4, title: "Software Development Engineering", 
+    skills: "Jira, Jenkins, Java, real-time testing"},
+    {key : 5, title: "Software Developer", 
     company: "Neusoft", duration: "2019.1 - 2019.4",
     skills: "Java, Spring Boot, mySQL"},
 ]
 
 function brief(){
-    return "Skilled in Java, JavaScript, SpringBoot, Vue, React. Worked as web developer for 3 co-op terms. Love to learn new stuff and make contributions :)"; 
+    return "Skilled in Java, JavaScript, SpringBoot, Vue, React. Worked as software developer for 3 co-op terms. Love to learn new stuff and make contributions :)"; 
 }
 function education(){
     return ([
@@ -32,6 +35,7 @@ function education(){
 function project(){
     return ([
         {key: 0, field: "This resume webpage", breif: "This webpage is built with React and Bootstrap, finished in May 2022 for applying jobs.", skills:["JavaScript","React"], source:"https://github.com/yiruW/yiru-cv"},
+        {key: 1, field: "Image detection", breif: "This project is used to detect emergency vehicle by flashing lights.", skills: ["Python", "OpenCV"], source: "https://github.com/yiruW/flashing-light-detection.git"},
         {key: 2, field: "Issue tracking webpage", breif: "The issue tracking webpage is similar to Jira, but only accept bug reports from customers who subscribe products. Mails will automatically sent to customers if new build is released, and parsed to create new bug tickets when received from users.", skills:["Java", "JavaScript", "SpringBoot", "Vue", "AmazonSaas"]},
         {key: 3, field: "Real-time OS", breif: "This project is a Linux system that handle instructions between kernel mode and user mode. The system will handle system call or any instructions, and execute memory allocations.", skills:["C"], source:"https://github.com/yiruW/realTimeOS"},
         {key: 4, field: "Product Website", breif: "A simple webpage display a product introduction.", skills:["JavaScript", "HTML", "CSS", "PS"], source:"https://github.com/yiruW/appLeaflet"}
